7666a330367c931ea2ea5b0bc1169c6416ebb438
juanichazo <jichazo@itba.edu.ar>
2026-06-18 00:13:26 +0000

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Default values work on localhost:3000 and foomedical.com
+// Replace these values with your own values for production
+export const MEDPLUM_BASE_URL = 'https://api.medplum.com.ar/';
+export const MEDPLUM_PROJECT_ID = '52fc66e7-799c-4a80-bc2a-e051ee20d2e7';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '221025255746-6uf0ahg713rdbk2dl6ab5bv2bg1qd3oc.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6Lf5xSQtAAAAAHrOw9qtUhyjDyMfEzXqZiDXVbZP';
+export const MEDPLUM_CLIENT_ID = '';
---
+++ src/main.tsx
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { Notifications } from '@mantine/notifications';
+import '@mantine/notifications/styles.css';
+import { MedplumClient } from '@medplum/core';
+import { MedplumProvider } from '@medplum/react';
+import '@medplum/react/styles.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { App } from './App';
+import {
+  MEDPLUM_BASE_URL,
+  MEDPLUM_PROJECT_ID,
+  MEDPLUM_GOOGLE_CLIENT_ID,
+  MEDPLUM_RECAPTCHA_SITE_KEY,
+  MEDPLUM_CLIENT_ID,
+} from './config';
+
+const medplum = new MedplumClient({
+  onUnauthenticated: () => (window.location.href = '/'),
+  baseUrl: MEDPLUM_BASE_URL,
+});
+
+const theme = createTheme({
+  primaryColor: 'teal',
+  primaryShade: 8,
+  fontSizes: {
+    xs: '0.6875rem',
+    sm: '0.875rem',
+    md: '0.875rem',
+    lg: '1rem',
+    xl: '1.125rem',
+  },
+  components: {
+    Container: {
+      defaultProps: {
+        size: 1200,
+      },
+    },
+  },
+});
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <MedplumProvider medplum={medplum}>
+        <MantineProvider theme={theme}>
+          <Notifications />
+          <App />
+        </MantineProvider>
+      </MedplumProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
